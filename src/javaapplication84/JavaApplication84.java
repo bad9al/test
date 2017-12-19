d83ed73a20932892946e4a80aa3792a5138c0a3e
Predavač <profesor@bmu.internal>
2017-12-19 15:14:36 +0100

    println

--- src/javaapplication84/JavaApplication84.java
+++ src/javaapplication84/JavaApplication84.java
@@ -17,7 +17,8 @@ public class JavaApplication84
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        System.out.println("test");
+                
     }
     
 }
